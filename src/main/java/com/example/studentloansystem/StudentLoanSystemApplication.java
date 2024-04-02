970c882860b281248a4c697922f899ff178bf90d
mohamadMahdiBeigi <mohamadinfernal@gmail.com>
2024-04-03 01:14:07 +0330

    open project and add dependencies to pom

---
+++ src/main/java/com/example/studentloansystem/StudentLoanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentloansystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoanSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentLoanSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentloansystem/StudentLoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentloansystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoanSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
