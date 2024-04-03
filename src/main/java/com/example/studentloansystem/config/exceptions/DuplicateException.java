36e2863788b7c6e1a6397ef46b18665651b2c315
mohamadMahdiBeigi <mohamadinfernal@gmail.com>
2024-04-03 20:38:00 +0330

    add customExceptions

---
+++ src/main/java/com/example/studentloansystem/config/exceptions/DuplicateException.java
@@ -0,0 +1,12 @@
+package com.example.studentloansystem.config.exceptions;
+
+public class DuplicateException extends RuntimeException{
+
+    public DuplicateException(String message) {
+        super(message);
+    }
+
+    public DuplicateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
